2c050e4da695c6c51563b8541738e0d8fafb0dd0
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-04-25 20:29:51 +0300

    add models to autoservice

--- autoservice/models.py
+++ autoservice/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class CarModel(models.Model):
+    car_model_id = models.AutoField(primary_key=True)
+    brand = models.CharField("Brand", max_length=100)
+    car_model = models.CharField("Car model", max_length=100)
+    production_year = models.DateField("Made on: ", null=True)
+    engine = models.CharField("Engine", max_length=100)
+
+    def __str__(self):
+        return f"{self.brand} - {self.car_model}"
+
+    # TODO - Class Meta: verbose_name + verbose_plural
+
+
+class Car(models.Model):
+    car_id = models.AutoField(primary_key=True)
+    car_model = models.ForeignKey(CarModel, on_delete=models.SET_NULL, null=True)
+    plate_number = models.CharField("Plate number", max_length=20)
+    vin_number = models.CharField("VIN", max_length=100)
+    client = models.CharField("Client", max_length=100)
+
+    def __str__(self):
+        return f"{self.client} - {self.car_model} - {self.plate_number} - {self.vin_number}"
+
+
+class Service(models.Model):
+    service_id = models.AutoField(primary_key=True)
+    service_name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.service_name}"
+
+
+class ServicePrice(models.Model):
+    service_price_id = models.AutoField(primary_key=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    cars = models.ManyToManyField(CarModel)
+    price = models.FloatField("Price")
+
+    def __str__(self):
+        return f"{self.service} - {self.price}"
+
+
+# TODO - prideti manytomany field i admina per atskira method
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_date = models.DateTimeField(default=datetime.datetime.now())
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    total_price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.car} - {self.total_price} - {self.order_date}"
+
+class OrderList(models.Model):
+    order_list_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+     def __str__(self):
+         return f"{self.service} - {self.quantity} - {self.price}"
