f7fb56c829e998c2e8bde107eb6c6235a4c76859
zvilkauskas <zilvinas.vilkauskas@gmail.com>
2023-04-27 22:04:07 +0300

    modify admin and models

--- autoservice/models.py
+++ autoservice/models.py
@@ -32,6 +32,7 @@ class Car(models.Model):
         verbose_name = 'Car'
         verbose_name_plural = 'Cars'
 
+
 class Service(models.Model):
     service_id = models.AutoField(primary_key=True)
     service_name = models.CharField(max_length=500)
@@ -42,6 +43,7 @@ class Service(models.Model):
     class Meta:
         verbose_name = 'Service'
         verbose_name_plural = 'Services'
+
 
 class ServicePrice(models.Model):
     service_price_id = models.AutoField(primary_key=True)
@@ -57,30 +59,40 @@ class ServicePrice(models.Model):
         verbose_name_plural = 'Service prices'
 
 
-# TODO - prideti manytomany field i admina per atskira method
-
-class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
+# Uzsakymas
+class OrderList(models.Model):
+    """Order List which is connected to an order. Representing the visit to an autoservice shope and the total order placed."""
+    order_list_id = models.AutoField(primary_key=True)
     order_date = models.DateTimeField(default=timezone.now)
     car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
     total_price = models.FloatField()
 
     def __str__(self):
-        return f"{self.car} - {self.service} - {self.total_price} - {self.order_date}"
+        return f"{self.car} - {self.order_date} - {self.total_price}"
+
+    class Meta:
+        verbose_name = 'Order List'
+        verbose_name_plural = 'Order Lists'
+
+
+# TODO - prideti manytomany field i admina per atskira method
+# Uzsakymo eilute
+class Order(models.Model):
+    """Order which is connected to an order list. Representing one service/thing bought."""
+    order_id = models.AutoField(primary_key=True)
+    order_list_id = models.ForeignKey(OrderList, on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.order_list_id} - {self.service} - {self.quantity} - {self.price}"
 
     class Meta:
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
 
-# FIXME: Is this logical?
-# class OrderInstance(models.Model):
-#     order_list_id = models.AutoField(primary_key=True)
-#     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-#     service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
-#     quantity = models.IntegerField()
-#     price = models.FloatField()
-#
-#     def __str__(self):
-#         return f"{self.service} - {self.quantity} - {self.price}"
+
+# order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+# service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+# quantity = models.IntegerField()
